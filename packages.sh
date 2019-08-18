30da0fdaf8ad9be9bb69c0c005329c334aee92d4
Hector Vido <hector_vido@yahoo.com.br>
2019-08-18 00:01:42 -0300

    Adicionado .profile e configurado sua copia

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany geany-plugins x11-xserver-utils
-cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources /home/developer/
+cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources .profile /home/developer/
 chown -R developer: /home/developer/
