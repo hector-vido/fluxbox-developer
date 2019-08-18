4ed2e49a279f0b1ce9c824030d7e3e29133a2387
Hector Vido <hector_vido@yahoo.com.br>
2019-08-17 23:53:37 -0300

    Adicionado a cópia dos arquivos para a home do ususário

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany geany-plugins
+cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources /home/developer/
+chown -R developer: /home/developer/
