bd527a5c337c0f871f098c42aa677e80623b6d1b
Hector Vido <hector_vido@yahoo.com.br>
2019-08-18 02:03:33 -0300

    Tentando automatizar todo o processo

--- transfer.sh
+++ transfer.sh
@@ -22,14 +22,26 @@ mount $1 /mnt
 cd /
 for X in $(ls -1 /); do
 	if [ -z "$(echo $X | grep -E 'mnt|proc|sys|dev')" ]; then
-		cp -av /$X /mnt
+		cp -av /$X /mnt/
 	fi
 done
+
+SDA1="$(blkid /dev/sda1 | sed -En 's|.*"(\w*-\w*-\w*-\w*-\w*)".*|\1|p')"
+SDA2="$(blkid /dev/sda2 | sed -En 's|.*"(\w*-\w*-\w*-\w*-\w*)".*|\1|p')"
+SDB1="$(blkid /dev/sdb1 | sed -En 's|.*"(\w*-\w*-\w*-\w*-\w*)".*|\1|p')"
+SDB2="$(blkid /dev/sdb2 | sed -En 's|.*"(\w*-\w*-\w*-\w*-\w*)".*|\1|p')"
+
+sed -Ei "s/$SDA1/$SDB1" /mnt/etc/fstab
+sed -Ei "s/$SDA2/$SDB2" /mnt/etc/fstab
+
+sed -Ei "s/$SDA1/$SDB1" /mnt/etc/initramfs-tools/conf.d/resume
 
 mkdir -p /mnt/{dev,proc,sys}
 mount --bind /dev /mnt/dev
 mount --bind /proc /mnt/proc
 mount --bind /sys /mnt/sys
+
+chroot /mnt
 
 cat <<EOF
 Os arquivos foram copiados. Os próximos passos são os seguintes:
