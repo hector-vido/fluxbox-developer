152894fc08e2cc4062a81223a7ef45c5a1f92f9a
Hector Vido <hector_vido@yahoo.com.br>
2019-08-27 23:21:23 -0300

    Melhorias no desktop, adição do pacote do php, adição do profile do firefox

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany geany-plugins x11-xserver-utils
-cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources .profile /home/developer/
+apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany geany-plugins x11-xserver-utils php-cli
+cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources .profile .mozilla /home/developer/
 chown -R developer: /home/developer/
