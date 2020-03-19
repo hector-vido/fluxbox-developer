166810dcab11462d9e7079cb09bb11705247f9ac
Hector Vido <hector_vido@yahoo.com.br>
2020-03-19 18:19:55 -0300

    Copiando arquivos de instalação

--- configure.sh
+++ configure.sh
@@ -4,5 +4,8 @@ apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany g
 cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources .profile .mozilla /home/developer/
 chown -R developer: /home/developer/
 
+cp bin/install-* /usr/local/bin/
+chmod +x /usr/local/bin/install-*
+
 sed -i 's,%sudo.*,%sudo ALL=(ALL:ALL) NOPASSWD:ALL,' /etc/sudoers
 gpasswd -a developer sudo
