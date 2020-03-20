77983cd383fa6720044312cd64732fc1d2f6e6b0
Hector Vido <hector_vido@yahoo.com.br>
2020-03-20 11:52:37 -0300

    Adicionado o autologin

--- configure.sh
+++ configure.sh
@@ -4,6 +4,9 @@ apt-get install -y xinit fluxbox git vim sakura idesk thunar firefox-esr geany g
 cp -r .fluxbox/ .config/ .ideskrc .idesktop/ .Xresources .profile .mozilla /home/developer/
 chown -R developer: /home/developer/
 
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo 'ExecStart=-/sbin/agetty --autologin developer --noclear %I 38400 linux' > /etc/systemd/system/getty@tty1.service.d/override.conf
+
 cp bin/install-* /usr/local/bin/
 chmod +x /usr/local/bin/install-*
 
