8e1afa5b8570e95ceebd03a1c3218fa0c6c20ee6
Hector Vido <39673799+hector-vido@users.noreply.github.com>
2020-09-22 14:51:04 -0300

    Melhorado script para criar novo disco

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	cat <<EOF
+        cat <<EOF
 Exemplo:
 
 bash transfer.sh /dev/sdb2
@@ -9,13 +9,22 @@ bash transfer.sh /dev/sdb2
 Especifique a partição para onde os dados serão copiados.
 Não é preciso montar a partição, apenas formatá-la.
 EOF
-	exit 1
+        exit 1
 fi
 
 if [ ! -b $1 ]; then
-	echo "Dispositivo $1 não existe"
-	exit 2
+        echo "Dispositivo $1 não existe"
+        exit 2
 fi
+
+partx -l /dev/sda | while read LINE; do
+        N=$(echo $LINE | awk '{print substr($2,1,1)}')
+        START=$(echo $LINE | awk '{print substr($3,1,length($3)-1)}')
+        END=$(echo $LINE | awk '{print $4}')
+        echo -e "n\np\n$N\n$START\n$END\nw" | fdisk $1
+done
+mkfs.ext4 -L debian /dev/sdb1
+mkswap -L swap /dev/sdb2
 
 sed -i 's/MODULES=.*/MODULES=dep/' /etc/initramfs-tools/initramfs.conf
 sed -i 's/COMPRESS=.*/COMPRESS=xz/' /etc/initramfs-tools/initramfs.conf
@@ -25,9 +34,9 @@ mount $1 /mnt
 
 cd /
 for X in $(ls -1 /); do
-	if [ -z "$(echo $X | grep -E 'mnt|proc|sys|dev')" ]; then
-		cp -av /$X /mnt
-	fi
+        if [ -z "$(echo $X | grep -E 'mnt|proc|sys|dev')" ]; then
+                cp -av /$X /mnt
+        fi
 done
 
 mkdir -p /mnt/{dev,proc,sys}
@@ -35,13 +44,15 @@ mount --bind /dev /mnt/dev
 mount --bind /proc /mnt/proc
 mount --bind /sys /mnt/sys
 
-cat <<EOF
-Os arquivos foram copiados. Os próximos passos são os seguintes:
-1. chroot /mnt
-2. blkid /dev/<dispositivos> ex: /dev/sdb1 /dev/sdb2
-3. vim /etc/fstab
-4. vim /etc/initramfs-tools/conf.d/resume - alterar UUID
-5. grub-install /dev/<dispositivo> ex: /dev/sdb
-6. update-grub
-7. update-initramfs -u
+cat > /mnt/update.sh <<EOF
+        BLKID=\$(blkid /dev/sdb1 | grep -Eo '[-0-9a-z]{36}')
+        sed -Ei "s,[-0-9a-z]{36}(.*ext4),\$BLKID\1," /etc/fstab
+        BLKID=\$(blkid /dev/sdb2 | grep -Eo '[-0-9a-z]{36}')
+        sed -Ei "s,[-0-9a-z]{36}(.*swap),\$BLKID\1," /etc/fstab
+        sed -Ei "s,[-0-9a-z]{36},\$BLKID," /etc/initramfs-tools/conf.d/resume
+        grub-install $1
+        update-grub
+        update-initramfs -u
 EOF
+
+chroot /mnt
